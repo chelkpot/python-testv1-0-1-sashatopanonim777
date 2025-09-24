8e9f27e37d4fd45e7b1d564f6b97c2878658c3d6
student <student@corp.kpiot.ru>
2025-09-24 09:16:07 +0500

    решил задачи

--- tasks/task5.py
+++ tasks/task5.py
@@ -4,7 +4,9 @@
 
 def solve():
 # Ниже пишите решение задачии(Обязательно поставьте четыре пробела после функции Solve())
-
+    numbers = list(map(int, input("Введите числа: ").split()))
+    squares = [n ** 2 for n in numbers]
+    print("Результат:", *squares)
 
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
